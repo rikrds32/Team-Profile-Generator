45085b57cb691a80a1de30db25c8e4b33996b4c0
Ricardo Salazar <no-replay>
2021-07-11 21:05:10 -0500

    finish renderer file

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const Manager = require("./lib/Manager");
-const Engenieer = require("./lib/Engineer");
+const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const Engineer = require("./lib/Engineer");
 const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "team.html");
-const team = []
 const render = require("./lib/htmlRenderer");
+const team = []
+
 
 const questions = [
     {
         type: "list",
         messege: "What is the employee role?",
-        choices: ["manager", "intern", "engineer"],
+        choices: ["Manager", "Intern", "Engineer"],
         name: "role"
     },
     {
@@ -36,19 +36,19 @@ const questions = [
         type: "input",
         message: "what is the office number?",
         name: "oficceNumber",
-        when: (answer) => answer.role === "manager"
+        when: (answer) => answer.role === "Manager"
     },
     {
         type: "input",
         message: "what is the github name?",
         name: "github",
-        when: (answer) => answer.role === "engineer"
+        when: (answer) => answer.role === "Engineer"
     },
     {
         tyoe: "input",
         message: "what the school name?",
         name: "school",
-        when: (answer) => answer.role === "intern"
+        when: (answer) => answer.role === "Intern"
     },
     {
         type: "list",
@@ -60,10 +60,10 @@ const questions = [
 
 function showQuestions() {
     inquirer.prompt(questions).then(res => {
-        if (res.role === "manager") {
+        if (res.role === "Manager") {
             const manager = new Manager(res.name, res.id, res.email, res.office);
             team.push(manager);
-        } else if (res.role === "engineer") {
+        } else if (res.role === "Engineer") {
             const engineer = new Engineer(res.name, res.id, res.email, res.github);
             team.push(engineer);
         } else {
